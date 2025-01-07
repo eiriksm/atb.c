1f7347573ca71c18a251733474bf59a0e775cee0
Eirik Stanghelle Morland <eirik@morland.no>
2025-01-07 19:11:55 +0100

    Update Number9.c

--- src/Number9.c
+++ src/Number9.c
@@ -1,6 +1,18 @@
 #include <string.h>
+#include <stdio.h>
 
 const char *timeStrings[] = {
         "19:12", 
         "19:42", 
+};
+
+// Define the structure for a key-value pair
+typedef struct {
+    int key;   // Key (integer index)
+    int value; // Value (integer value)
+} KeyValuePair;
+
+// Define an array with one key-value pair
+KeyValuePair stopOffsets[] = {
+  {71779, 4}
 };
