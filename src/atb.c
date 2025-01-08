de8073d191d22aec41ff7298d7f92470a7bd72c0
Eirik Stanghelle Morland <eirik@morland.no>
2025-01-08 19:40:43 +0100

    Delete `src/Number9.c` and `test/TestNumber9.c`
    **Implement function in `src/atb.c`**
    * Add `atb_get_next_departures_route_9` function to dynamically get next departures for route 9
    * Integrate the function with existing code
    
    **Add test in `test/Testatb.c`**
    * Add `test_get_next_departures_route_9` to test the new function
    * Cover all possible cases for different stops

--- src/atb.c
+++ src/atb.c
@@ -16,3 +16,20 @@ DepartureTimes atb_get_next_departures(int timestamp, char route, char stop_id) 
   }
   return result;
 }
+
+DepartureTimes atb_get_next_departures_route_9(int timestamp, char stop_id) {
+  DepartureTimes result;
+  result.count = 0;
+
+  if (stop_id < 1 || stop_id > 5) {
+    return result;
+  }
+
+  for (int i = 0; i < MAX_DEPARTURES; i++) {
+    if (route_9_schedule[stop_id - 1][i] >= timestamp) {
+      result.departure_times[result.count++] = route_9_schedule[stop_id - 1][i];
+    }
+  }
+
+  return result;
+}
