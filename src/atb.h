46ac6fd40f47381651e7efec576b3f1dddd4258d
Eirik S. Morland <eirik@morland.no>
2025-01-11 12:17:59 +0100

    So much better and easier

--- src/atb.h
+++ src/atb.h
@@ -4,18 +4,37 @@
 #define MAX_DEPARTURES 5
 
 typedef struct {
-    int departure_times[MAX_DEPARTURES];
+    char* departure_times[MAX_DEPARTURES];
     int count;
 } DepartureTimes;
 
-// Schedule information for route 9
-static const char* route_9_schedule[][MAX_DEPARTURES] = {
-    {"06:00", "06:30", "07:00", "07:30", "08:00"}  // Stop 1
+typedef struct {
+    int resultSet[MAX_DEPARTURES];
+} ResultSet;
+
+typedef struct {
+    char* stop_id;
+    char* route;
+    int offset;
+} StopOffset;
+
+typedef struct route {
+    char* route;
+    int day_id;
+    DepartureTimes departureTimes;
+} Route;
+
+static const Route schedules[1] = {
+    {"9", 5, {{"14:57", "15:12", "15:27", "15:42", "15:57"}, 5}}
 };
 
+static const StopOffset stop_offsets[1] = {
+    {"71779", "9", 5}
+};
+
+
 // Function prototypes (declarations)
-int atb_get_next_departure(int timestamp, char route, char stop_id);
-DepartureTimes atb_get_next_departures(int timestamp, char route, char stop_id);
-DepartureTimes atb_get_next_departures_route_9(int timestamp, char stop_id);
+int atb_get_next_departure(int timestamp, char* route, char* stop_id);
+ResultSet atb_get_next_departures(int timestamp, char* route, char* stop_id);
 
 #endif
--- test/Testatb.c
+++ test/Testatb.c
@@ -8,80 +8,16 @@ void tearDown(void) {
 }
 
 void test_get_next_departures_route_9(void) {
-    DepartureTimes result;
+    ResultSet result;
 
-    // Test for stop 1
-    result = atb_get_next_departures_route_9(605, 1);
-    TEST_ASSERT_EQUAL(4, result.count);
-    TEST_ASSERT_EQUAL(630, result.departure_times[0]);
-    TEST_ASSERT_EQUAL(700, result.departure_times[1]);
-    TEST_ASSERT_EQUAL(730, result.departure_times[2]);
-    TEST_ASSERT_EQUAL(800, result.departure_times[3]);
-
-    // Test for stop 2
-    result = atb_get_next_departures_route_9(635, 2);
-    TEST_ASSERT_EQUAL(4, result.count);
-    TEST_ASSERT_EQUAL(705, result.departure_times[0]);
-    TEST_ASSERT_EQUAL(735, result.departure_times[1]);
-    TEST_ASSERT_EQUAL(805, result.departure_times[2]);
-    TEST_ASSERT_EQUAL(0, result.departure_times[3]);
-
-    // Test for stop 3
-    result = atb_get_next_departures_route_9(640, 3);
-    TEST_ASSERT_EQUAL(4, result.count);
-    TEST_ASSERT_EQUAL(710, result.departure_times[0]);
-    TEST_ASSERT_EQUAL(740, result.departure_times[1]);
-    TEST_ASSERT_EQUAL(810, result.departure_times[2]);
-    TEST_ASSERT_EQUAL(0, result.departure_times[3]);
-
-    // Test for stop 4
-    result = atb_get_next_departures_route_9(645, 4);
-    TEST_ASSERT_EQUAL(4, result.count);
-    TEST_ASSERT_EQUAL(715, result.departure_times[0]);
-    TEST_ASSERT_EQUAL(745, result.departure_times[1]);
-    TEST_ASSERT_EQUAL(815, result.departure_times[2]);
-    TEST_ASSERT_EQUAL(0, result.departure_times[3]);
-
-    // Test for stop 5
-    result = atb_get_next_departures_route_9(650, 5);
-    TEST_ASSERT_EQUAL(4, result.count);
-    TEST_ASSERT_EQUAL(720, result.departure_times[0]);
-    TEST_ASSERT_EQUAL(750, result.departure_times[1]);
-    TEST_ASSERT_EQUAL(820, result.departure_times[2]);
-    TEST_ASSERT_EQUAL(0, result.departure_times[3]);
-}
-
-void test_get_next_departure(void) {
-    int result;
-
-    // Test for route 9, stop 1
-    result = atb_get_next_departure(605, '9', 1);
-    TEST_ASSERT_EQUAL(630, result);
-
-    // Test for route 9, stop 2
-    result = atb_get_next_departure(635, '9', 2);
-    TEST_ASSERT_EQUAL(705, result);
-
-    // Test for route 9, stop 3
-    result = atb_get_next_departure(640, '9', 3);
-    TEST_ASSERT_EQUAL(710, result);
-
-    // Test for route 9, stop 4
-    result = atb_get_next_departure(645, '9', 4);
-    TEST_ASSERT_EQUAL(715, result);
-
-    // Test for route 9, stop 5
-    result = atb_get_next_departure(650, '9', 5);
-    TEST_ASSERT_EQUAL(720, result);
-
-    // Test for no next departure
-    result = atb_get_next_departure(820, '9', 5);
-    TEST_ASSERT_EQUAL(-1, result);
+    // Test for stop 71779. This is a Wednesday at 14:00:40. The next departure
+    // should be 14:02.
+    result = atb_get_next_departures(1736344840, "9", "71779");
+    TEST_ASSERT_EQUAL(1736344920, result.resultSet[0]);
 }
 
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_get_next_departures_route_9);
-  RUN_TEST(test_get_next_departure);
   return UNITY_END();
 }
