a062c349cf3818753a787527e000337dfa578c99
Eirik Stanghelle Morland <eirik@morland.no>
2025-01-12 20:31:19 +0100

    Easier and more verbose support of directions (#15)

--- test/Testatb.c
+++ test/Testatb.c
@@ -12,27 +12,29 @@ void test_get_next_departures_route_9(void) {
 
     // Test for stop 71779. This is a Wednesday at 14:00:40. The next departure
     // should be 14:02.
-    result = atb_get_next_departures(1736344840, "9", "71779");
+    static char ROUTE_TO_TEST[] = "09_2";
+    static char STOP_ID[] = "71779";
+    result = atb_get_next_departures(1736344840, ROUTE_TO_TEST, STOP_ID);
     TEST_ASSERT_EQUAL(1736344920, result.resultSet[0]);
 
     // This is a Saturday at 13:37. The next departure should be 13:47.
-    int next = atb_get_next_departure(1736599075, "9", "71779");
+    int next = atb_get_next_departure(1736599075, ROUTE_TO_TEST, STOP_ID);
     TEST_ASSERT_EQUAL(1736599620, next);
 
     // This is friday the last tram.
-    result = atb_get_next_departures(1736548975, "9", "71779");
+    result = atb_get_next_departures(1736548975, ROUTE_TO_TEST, STOP_ID);
     TEST_ASSERT_EQUAL(1736549220, result.resultSet[0]);
 
     // Last on Sat.
-    result = atb_get_next_departures(1736635375, "9", "71779");
+    result = atb_get_next_departures(1736635375, ROUTE_TO_TEST, STOP_ID);
     TEST_ASSERT_EQUAL(1736635620, result.resultSet[0]);
 
     // 12.30 on Sunday.
-    result = atb_get_next_departures(1736681400, "9", "71779");
+    result = atb_get_next_departures(1736681400, ROUTE_TO_TEST, STOP_ID);
     TEST_ASSERT_EQUAL(1736682420, result.resultSet[0]);
 
     // Last on Sunday.
-    result = atb_get_next_departures(1736721000, "9", "71779");
+    result = atb_get_next_departures(1736721000, ROUTE_TO_TEST, STOP_ID);
     TEST_ASSERT_EQUAL(1736722020, result.resultSet[0]);
 }
 
